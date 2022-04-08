05ab1fbe940d9d550bedadadd1ac22bb4379de61
CimarSantos <francimarlsantos@outlook.com>
2022-04-08 17:13:43 -0300

    Distribuição aleatória do baralho

--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
+const baralho = "<div class='carta' onclick='viraCarta(this)'></div> ";
+const distro = document.querySelector(".espaco-baralho");
+
 let qtdCarta = prompt("De 4 à 14, com quantas cartas quer jogar?");
 
 while (qtdCarta % 2 !== 0 || qtdCarta < 4 || qtdCarta > 14) {
     qtdCarta = prompt("Só são aceitos números pares, digite um número de 4 à 14");
+}
+
+for (let i = 0; i < (qtdCarta / 2); i++) {
+    for (let j = 0; j < 2; j++) {
+        distro.innerHTML += `${baralho}`;
+    }
+}
+
+function viraCarta(carta) {
+
+    carta.classList.add("comp" + numAleatorio());
+}
+
+function numAleatorio(min, max) {
+    min = Math.ceil(1);
+    max = Math.floor(7);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
